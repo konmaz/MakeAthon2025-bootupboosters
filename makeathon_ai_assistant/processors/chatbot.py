9db21646c6c47cde82cb6fda1f1ff5c25fa65bd3
ntua-el21608 <el21608@mail.ntua.gr>
2025-05-24 17:17:25 +0300

    chatbot fix

--- makeathon_ai_assistant/processors/chatbot.py
+++ makeathon_ai_assistant/processors/chatbot.py
@@ -12,7 +12,7 @@ os.environ["OPENAI_API_TYPE"] = "azure"
 os.environ["OPENAI_API_BASE"] = os.getenv("AZURE_OPENAI_ENDPOINT")
 os.environ["OPENAI_API_VERSION"] = "2023-05-15"
 
-client = AzureOpenAI()
+client = AzureOpenAI(api_key=os.getenv("AZURE_OPENAI_KEY"))
 
 persist_directory = "./chroma_db"
 
